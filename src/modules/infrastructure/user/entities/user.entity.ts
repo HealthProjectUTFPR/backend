b8eb2c4aa9357224951953615158c9c5e4852952
Isaac S. de Almeida <94393622+IsaacDMz@users.noreply.github.com>
2022-10-12 15:00:33 -0300

    Editar usuário Fixes #82

--- src/modules/infrastructure/user/entities/user.entity.ts
+++ src/modules/infrastructure/user/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { BaseEntity } from 'src/common/entities/base.entity';
 import { generateHash } from 'src/common/utils/hash.util';
 import { TodoItem } from 'src/modules/domain/todo-item/entities/todo-item.entity';
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany} from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, CreateDateColumn} from 'typeorm';
 
 @Entity()
 export class User extends BaseEntity {
