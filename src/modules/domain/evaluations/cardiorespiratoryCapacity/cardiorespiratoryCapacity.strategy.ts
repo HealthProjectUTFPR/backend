0bcde4b9a54e158a92761b98fe7f290f89b78b7b
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2022-10-25 17:50:26 -0300

    feat: finalizando metodo create strategy eval. cardio

--- src/modules/domain/evaluations/cardiorespiratoryCapacity/cardiorespiratoryCapacity.strategy.ts
+++ src/modules/domain/evaluations/cardiorespiratoryCapacity/cardiorespiratoryCapacity.strategy.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Evaluation } from '../entities/evaluation.entity';
 import { CardiorespiratoryCapacityFactory } from './cardiorespiratoryCapacity.factory';
 import { CardiorespiratoryCapacityDto } from './dto/cardiorespiratoryCapacity.dto';
-import { calculateCardiorespiratoryCapacityResult } from './helper/calculateResult';
-import { calculateVO2LMin } from './helper/calculateVO2Lmin';
-import { calculateVO2MlKg } from './helper/calculateVO2MlKg';
+import { calculateCardiorespiratoryCapacityResult } from './helpers/calculateResult';
+import { calculateVO2LMin } from './helpers/calculateVO2Lmin';
+import { calculateVO2MlKg } from './helpers/calculateVO2MlKg';
 import { ICardiorespiratoryCapacity } from './interface/cardiorespiratoryCapacity.interface';
 
 @Injectable()
@@ -26,12 +26,21 @@ export class CardiorespiratoryCapacityStrategy {
     return calculateCardiorespiratoryCapacityResult({ sex, vo2MlKG });
   }
 
+  private validateResult(
+    result: string,
+    input: Partial<ICardiorespiratoryCapacity>,
+  ) {
+    const recalculationResult = this.recalculateResult(input);
+
+    return recalculationResult === result;
+  }
+
   async create(input: CardiorespiratoryCapacityDto): Promise<Evaluation> {
     const sex = 'Homem';
     const age = 70;
     const { date, weight, time, finalFC, vo2Lmin, vo2MlKG, result } = input;
 
-    const recalculationResult = this.recalculateResult({
+    const isResultValid = this.validateResult(result, {
       weight,
       finalFC,
       time,
@@ -39,6 +48,19 @@ export class CardiorespiratoryCapacityStrategy {
       sex,
     });
 
-    return await this.cardiorespiratoryCapacityFactory.createOrUpdate(input);
+    if (!isResultValid)
+      throw new BadRequestException(
+        'Resultado inválido de acordo com os dados repassados!',
+      );
+
+    return await this.cardiorespiratoryCapacityFactory.createOrUpdate({
+      date,
+      weight,
+      time,
+      finalFC,
+      vo2Lmin,
+      vo2MlKG,
+      result,
+    });
   }
 }
