946f853b3ff31a322c41cb204a8f4779f53ddb09
JessePires <jessepires2010@gmail.com>
2022-10-31 20:38:46 -0300

    fix: Removendo enum de data type inutilizado

--- src/modules/domain/evaluations/entities/field.entity.ts
+++ src/modules/domain/evaluations/entities/field.entity.ts
@@ -3,14 +3,6 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 import { Evaluation } from './evaluation.entity';
 import { TestBatteries } from './test-batteries.entity';
 
-export enum DataType {
-  Dt_int = 'int',
-  Dt_string = 'string',
-  Dt_float = 'float',
-  Dt_boolean = 'boolean',
-  Dt_date = 'date',
-}
-
 @Entity()
 export class Field extends BaseEntity {
   @Column()
@@ -20,11 +12,9 @@ export class Field extends BaseEntity {
   value: string;
 
   @Column({
-    type: 'enum',
-    enum: DataType,
-    default: DataType.Dt_string,
+    default: 'string',
   })
-  dataType: DataType;
+  dataType: string;
 
   @ManyToOne(() => Evaluation, (evaluation) => evaluation.fields)
   evaluationField: Evaluation;
