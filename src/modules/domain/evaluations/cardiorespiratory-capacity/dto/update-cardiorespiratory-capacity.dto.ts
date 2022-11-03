a848b7ecc906f1f600c40a5f2f67565b9ad6211c
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2022-11-02 23:16:28 -0300

    refactor: alterando dto update

--- src/modules/domain/evaluations/cardiorespiratory-capacity/dto/update-cardiorespiratory-capacity.dto.ts
+++ src/modules/domain/evaluations/cardiorespiratory-capacity/dto/update-cardiorespiratory-capacity.dto.ts
@@ -1,33 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
-import * as Joi from 'joi';
-import { JoiSchema } from 'nestjs-joi';
+import { CreateCardiorespiratoryCapacityDto } from './create-cardiorespiratory-capacity.dto';
 
-export class UpdateCardiorespiratoryCapacityDto {
-  @ApiProperty()
-  @JoiSchema(Joi.date().required())
-  date: Date;
-
-  @ApiProperty()
-  @JoiSchema(Joi.number().required())
-  weight: number;
-
-  @ApiProperty()
-  @JoiSchema(Joi.number().required())
-  time: number;
-
-  @ApiProperty()
-  @JoiSchema(Joi.number().required())
-  finalFC: number;
-
-  @ApiProperty()
-  @JoiSchema(Joi.number().required())
-  vo2Lmin: number;
-
-  @ApiProperty()
-  @JoiSchema(Joi.number().required())
-  vo2MlKG: number;
-
-  @ApiProperty()
-  @JoiSchema(Joi.string().required())
-  result: string;
-}
+export class UpdateCardiorespiratoryCapacityDto extends CreateCardiorespiratoryCapacityDto {}
