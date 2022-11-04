ee33054bf42b7c0e4729e1e5c1f2808a5ac031d8
Nakao <Matheus_K_Nakao@hotmail.com>
2022-11-04 15:07:36 -0300

    feat: geração da base para a avl de equilibrio

---
+++ src/modules/domain/evaluations/balance/interface/balance.interface.ts
@@ -0,0 +1,18 @@
+export interface IBalance {
+    total: number;
+    campo1: number;
+    campo2: number;
+    campo3: number;
+    campo4: number;
+    campo5: number;
+    campo6: number;
+    campo7: number;
+    campo8: number;
+    campo9: number;
+    campo10: number;
+    campo11: number;
+    campo12: number;
+    campo13: number;
+    campo14: number;
+    date: Date;
+}
