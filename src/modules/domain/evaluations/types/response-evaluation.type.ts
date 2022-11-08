504d5e4f4800d99825cb5f243b2985d1b474b817
Nakao <Matheus_K_Nakao@hotmail.com>
2022-11-08 13:31:34 -0300

    Fix: Melhorando a Syntax

--- src/modules/domain/evaluations/types/response-evaluation.type.ts
+++ src/modules/domain/evaluations/types/response-evaluation.type.ts
@@ -1,4 +1,6 @@
 import { GetCardiorespiratoryCapacityDto } from '../cardiorespiratory-capacity/dto/get-cardiorespiratory-capacity.dto';
 import { GetBalanceDto } from '../balance/dto/get-balance.dto';
 
-export type ResponseEvaluation = GetCardiorespiratoryCapacityDto | GetBalanceDto;
+export type ResponseEvaluation = 
+    | GetCardiorespiratoryCapacityDto
+    | GetBalanceDto;
