cde9184fb01fa3704f25b6114fee628d69c93147
LASeemund <seemund@alunos.utfpr.edu.br>
2022-11-10 15:11:58 -0300

    feat: criando os casos de test

--- src/modules/domain/evaluations/mini-cognition/mini-cognition.strategy.ts
+++ src/modules/domain/evaluations/mini-cognition/mini-cognition.strategy.ts
@@ -8,6 +8,7 @@ import { PaginationParams } from 'src/common/interfaces/pagination.interface';
 import { User } from 'src/modules/infrastructure/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { Student } from '../../student/entities/student.entity';
+import { parseType } from 'src/common/utils/parse-type.util';
 import { Evaluation } from '../entities/evaluation.entity';
 import { EvaluationOrderBy } from '../enums/order-by.enum';
 import { MiniCognitionFactory } from './mini-cognition.factory';
@@ -16,6 +17,8 @@ import { CreateMiniCognitionDto } from './dto/create-mini-cognition.dto';
 import { GetMiniCognitionDto } from './dto/get-mini-cognition.dto';
 import { GetAllMiniCognitionDto } from './dto/get-all-mini-cognition.dto';
 import { UpdateMiniCognitionDto } from './dto/update-mini-cognition.dto';
+import { IMiniCognition } from './interface/miniCognition.interface'
+import { validateResult } from './helper/validade-result-mini-cognition';
 
 @Injectable()
 export class MiniCognitionStrategy {
@@ -25,6 +28,26 @@ export class MiniCognitionStrategy {
   constructor(
     private readonly MiniCognitionFactory: MiniCognitionFactory,
   ) {}
+
+  private parseFieldsToCorrectType(
+    data: Evaluation,
+  ): GetMiniCognitionDto{
+    const { id, name, createdAt, updatedAt, result, deletedAt, fields } = data;
+    let parsedFields: Partial<IMiniCognition> = {};
+    fields.forEach(({ name, value, dataType }) => {
+      parsedFields[name] = parseType(dataType, value) === 'true';
+    });
+    parsedFields.result = result;
+    const returnedValues: GetMiniCognitionDto = {
+      id,
+      name,
+      createdAt,
+      updatedAt,
+      deletedAt,
+      ...parsedFields as CreateMiniCognitionDto,
+    };
+    return returnedValues;
+  }
 
   async create(
     input: CreateMiniCognitionDto,
@@ -38,10 +61,45 @@ export class MiniCognitionStrategy {
       throw new BadRequestException(validation.error.message);
     }
 
+    const validationResult = validateResult(input);
+    if (!validationResult) {
+      throw new NotFoundException(`Avaliação com o resultado errado`);
+    }
+
     return await this.MiniCognitionFactory.create(input, user, type, student);
   }
-  
+
   async update(
+    id: string,
+    type: string,
+    input: UpdateMiniCognitionDto,
+  ): Promise<GetMiniCognitionDto> {
+    const validation = MiniCognitionSchema.validate(input);
+
+    if (validation?.error) {
+      throw new BadRequestException(validation.error.message);
+    }
+
+    const evaluation = await this.evaluationRepository.findOne({
+      where: {
+        id,
+        deletedAt: null,
+      },
+      relations: ['fields', 'student'],
+    });
+
+    if (!evaluation) {
+      throw new NotFoundException(`Avaliação com id ${id} não encontrada`);
+    }
+
+    const validationResult = validateResult(input as CreateMiniCognitionDto);
+    if (!validationResult) {
+      throw new NotFoundException(`Avaliação com o resultado errado`);
+    }
+
+    return this.parseFieldsToCorrectType(evaluation);
+  }
+
   async getAll(
     orderBy: EvaluationOrderBy,
     paginationParams: PaginationParams,
@@ -60,4 +118,17 @@ export class MiniCognitionStrategy {
 
     return returnedData;
   }
-/*
+
+  async getByID(id: string): Promise<GetMiniCognitionDto> {
+    const evaluation = await this.evaluationRepository.findOne({
+      where: { id, deletedAt: null },
+      relations: ['fields'],
+    });
+
+    if (!evaluation) {
+      throw new NotFoundException(`Avaliação com id ${id} não encontrada`);
+    }
+    
+    return this.MiniCognitionFactory.getOne(evaluation);
+  }
+}
