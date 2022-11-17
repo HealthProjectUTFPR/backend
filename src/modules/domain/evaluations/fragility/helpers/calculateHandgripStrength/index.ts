d3b46dc37c1ef9ef092a8bc51f06870cb2edd1b9
JessePires <jessepires2010@gmail.com>
2022-11-16 22:20:31 -0300

    refactor: Simplificando função de cálculo de preensão manual

--- src/modules/domain/evaluations/fragility/helpers/calculateHandgripStrength/index.ts
+++ src/modules/domain/evaluations/fragility/helpers/calculateHandgripStrength/index.ts
@@ -9,38 +9,36 @@ const calculateHandgripStrength = ({
   imc,
   sex,
 }: ICalculateHandgripStrength) => {
-  let result = 0;
-
   if (sex === 'Homem') {
     if (handgripStrength <= 29 && imc <= 24) {
-      result = 1;
+      return 1;
     } else if (handgripStrength <= 30) {
       if (imc >= 24.1 && imc <= 26) {
-        result = 1;
+        return 1;
       } else if (imc >= 26.1 && imc <= 28) {
-        result = 1;
+        return 1;
       }
     } else if (handgripStrength <= 32 && imc >= 29) {
-      result = 1;
+      return 1;
     }
   } else if (sex === 'Mulher') {
     if (handgripStrength <= 17 && imc <= 23) {
-      result = 1;
+      return 1;
     } else if (handgripStrength <= 17.3) {
       if (imc >= 25.1 && imc <= 26) {
-        result = 1;
+        return 1;
       }
     }
     if (handgripStrength <= 18) {
       if (imc >= 26.1 && imc <= 29) {
-        result = 1;
+        return 1;
       }
     } else if (handgripStrength <= 21 && imc >= 29) {
-      result = 1;
+      return 1;
     }
   }
 
-  return result;
+  return 0;
 };
 
 export default calculateHandgripStrength;
