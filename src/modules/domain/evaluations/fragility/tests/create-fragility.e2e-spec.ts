4986be4fa1e345b560f011243b491cdf292e4985
JessePires <jessepires2010@gmail.com>
2022-11-18 14:10:24 -0300

    test: Adicionando teste de criação da avaliação

--- src/modules/domain/evaluations/fragility/tests/create-fragility.e2e-spec.ts
+++ src/modules/domain/evaluations/fragility/tests/create-fragility.e2e-spec.ts
@@ -9,6 +9,7 @@ import { EvaluationModule } from '../../evaluation.module';
 
 let app: INestApplication;
 let server: request.SuperTest<request.Test>;
+let token: string;
 let studentId: string;
 
 beforeAll(async () => {
@@ -21,4 +22,110 @@ beforeAll(async () => {
       StudentModule,
     ],
   }).compile();
+
+  app = module.createNestApplication();
+  await app.init();
+  server = request(app.getHttpServer());
+
+  await server
+    .post('/auth/register')
+    .send({ email: 'test@fragility.com', name: 'teste', password: '12345678' })
+    .expect(201);
+
+  const login = await server
+    .post('/auth/login')
+    .send({ email: 'test@fragility.com', password: '12345678' })
+    .expect(200);
+
+  token = login.text;
+
+  const student = await server
+    .post('/student/create')
+    .send({
+      name: 'Estudante',
+      sex: 'M',
+      breed: 'Branco',
+      stature: 192.5,
+      healthPlan: 'free',
+      emergencyContact: '449994484848',
+      contact: '449994484848',
+      address: 'Rua 123',
+      birthDate: '1980-10-12T03:00:00.000Z',
+      flag: true,
+    })
+    .set('Authorization', `Bearer ${token}`);
+
+  studentId = student.body.id;
+});
+
+afterAll(async () => {
+  await app.close();
+});
+
+describe('CRiar avaliações de Fragilidade', () => {
+  it(`/:studentId (CREATE) Falha na validação do resultado`, async () => {
+    return await server
+      .post(`/evaluation/${studentId}`)
+      .send({
+        type: 'fragilidade',
+        data: {
+          date: '2022-11-10T03:00:00.000Z',
+          weight: 59,
+          looseWeight: 4.6,
+          activityDifficultLastWeekFrequency: 5,
+          KeepGoingDifficultLastWeekFrequency: 5,
+          walkingDays: 2,
+          walkingMinutesPerDay: 30,
+          moderateActivityDays: 2,
+          moderateActivityMinutesPerDay: 30,
+          vigorousActivityDays: 2,
+          vigorousActivityMinutesPerDay: 30,
+          time: 30,
+          handgripStrength: 5,
+          imc: 26.2,
+          mets1: 198,
+          mets2: 240,
+          mets3: 480,
+          metsTotal: 918,
+          kcal: 902.7,
+          score: 5,
+          result: 'frágil',
+        },
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .expect(400);
+  });
+
+  it(`/:studentId (CREATE) Sucesso na criação`, async () => {
+    return await server
+      .post(`/evaluation/${studentId}`)
+      .send({
+        type: 'fragilidade',
+        data: {
+          date: '2022-11-10T03:00:00.000Z',
+          weight: 59,
+          looseWeight: 4.6,
+          activityDifficultLastWeekFrequency: 5,
+          KeepGoingDifficultLastWeekFrequency: 5,
+          walkingDays: 2,
+          walkingMinutesPerDay: 30,
+          moderateActivityDays: 2,
+          moderateActivityMinutesPerDay: 30,
+          vigorousActivityDays: 2,
+          vigorousActivityMinutesPerDay: 30,
+          time: 30,
+          handgripStrength: 5,
+          imc: 26.2,
+          mets1: 198,
+          mets2: 240,
+          mets3: 480,
+          metsTotal: 918,
+          kcal: 902.7,
+          score: 3,
+          result: 'frágil',
+        },
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .expect(201);
+  });
 });
