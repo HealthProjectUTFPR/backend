c5594d017e40a32d3595273fdd20cf677c4acb5d
JessePires <jessepires2010@gmail.com>
2022-11-30 22:19:12 -0300

    Revert "fix: Corrigindo helpers"
    This reverts commit bef2ac2721fe9426dc0ce66e95d39bb8a3ea21ec.

--- src/modules/domain/evaluations/fragility/fragility.strategy.ts
+++ src/modules/domain/evaluations/fragility/fragility.strategy.ts
@@ -132,7 +132,7 @@ export class FragilityStrategy {
   ): Promise<GetFragilityDTO> {
     const { sex: studentSex, stature } = student;
 
-    const sex = studentSex === 'M' ? 'Masculino' : 'Feminino';
+    const sex = studentSex === 'H' ? 'Homem' : 'Mulher';
 
     const {
       date,
@@ -244,7 +244,7 @@ export class FragilityStrategy {
 
     const { sex: studentSex, stature } = evaluation.student;
 
-    const sex = studentSex === 'M' ? 'Masculino' : 'Feminino';
+    const sex = studentSex === 'H' ? 'Homem' : 'Mulher';
 
     const {
       date,
